a2e8411be79f93fca0a6704cb993dc17635d8fd9
furkan-yigit <fyigit98@outlook.com>
2024-02-06 14:44:23 +0300

    first

--- KHP.Bll.IServices/IGidaService.cs
+++ KHP.Bll.IServices/IGidaService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KHP.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace KHP.Bll.IServices
 {
     public interface IGidaService
     {
+        List<GidaListVm> GetAll();
+        public List<GidaListVm> TarihlerArasiKategoriListeleme(DateTime baslangic, DateTime bitis);
     }
 }
--- KHP.ViewModels/GidaListVm.cs
+++ KHP.ViewModels/GidaListVm.cs
@@ -9,10 +9,12 @@ namespace KHP.ViewModels
 {
     public class GidaListVm
     {
+        public int Id { get; set; }
         public string Ad { get; set; }
         public decimal Kalori { get; set; }
         public GidaTurleri GidaTuru { get; set; }
 
         public int? OgunID { get; set; }
+        public DateTime OlusturulmaTarihi { get; set; }
     }
 }
